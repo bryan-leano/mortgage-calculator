e9dc250945ec4b1726e31fd752292764e64bcc53
Bryan Leanoe <bryan.leano@gmail.com>
2018-10-19 11:58:20 -0600

    add down payment option

--- mortgage-calculator.js
+++ mortgage-calculator.js
@@ -1,16 +1,19 @@
 //mortgage calculator
 
-var homePrice, mortgagePeriod, interest, monthlyMortgage;
+var homePrice, mortgagePeriod, interest, monthlyMortgage, downPayment;
+
+// add in down payment
 
 document.querySelector('.btn-calculate').addEventListener('click', function() {
   homePriceInput = document.querySelector('.home-price').value;
   mortgagePeriodInput = document.querySelector('.mortgage-period').value;
   interestInput = document.querySelector('.interest').value;
+  downPaymentInput = document.querySelector('.down-payment').value;
 
   if (homePriceInput && mortgagePeriodInput && interestInput) {
     calculate();
   } else {
-    //if there is one value not inputted, then do something else
+    console.log('Please fill out all values!')
   }
 
 });
@@ -19,10 +22,11 @@ function calculate() {
   interest = interestInput / 100;
   monthlyInterest = interest / 12;
   mortgagePeriod = mortgagePeriodInput * 12;
+  mortgageStartingValue = homePriceInput - downPaymentInput
 
-  monthlyMortgage = homePriceInput * ( monthlyInterest * ( Math.pow((1 + monthlyInterest), 
+  monthlyMortgage = mortgageStartingValue * ( monthlyInterest * ( Math.pow((1 + monthlyInterest), 
     mortgagePeriod) / ( Math.pow(( 1 + monthlyInterest ), mortgagePeriod ) - 1 )))
 
   console.log(monthlyMortgage);  
-
+  //add in display box
 };
