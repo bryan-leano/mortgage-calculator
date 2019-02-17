810c369710462c0b85ec5a3aaafd8ec94f2c0164
Bryan Leanoe <bryan.leano@gmail.com>
2019-02-17 13:03:16 -0700

    error display messages html

--- mortgage-calculator.js
+++ mortgage-calculator.js
@@ -12,19 +12,33 @@ document.querySelector('.btn-calculate').addEventListener('click', function() {
   if ( homePriceInput && mortgagePeriodInput && interestInput ) {
     calculate();
   } else if ( homePriceInput && mortgagePeriodInput ) {
-    console.log('Please fill out the Interest!');
+    //console.log('Please fill out the Interest!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Interest!`;
   } else if ( homePriceInput && interestInput ) {
-    console.log('Please fill out the Mortgage Period!');
+    //console.log('Please fill out the Mortgage Period!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Mortgage Period!`;
   } else if ( mortgagePeriodInput && interestInput ) {
-    console.log('Please fill out the Home Price!');
+    //console.log('Please fill out the Home Price!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Home Price!`;
   } else if ( homePriceInput ) {
-    console.log('Please fill out the Mortgage Period and Interest!');
+    //console.log('Please fill out the Mortgage Period and Interest!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Mortage Period & Interest!`;
   } else if (mortgagePeriodInput) {
-    console.log('Please fill out the Home Price and Interest!');
+    //console.log('Please fill out the Home Price and Interest!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Home Price & Interest!`;
   } else if ( interestInput ) {
-    console.log('Please fill out Home Price and Mortgage Period!');
+    //console.log('Please fill out Home Price and Mortgage Period!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Home Price & Mortgage Period!`;
   } else {
-    console.log('Please fill out all Home Price, Mortgage Period and Interest!');
+    //console.log('Please fill out all Home Price, Mortgage Period and Interest!');
+    var displayMortgage = document.getElementById('mortgage');
+    displayMortgage.innerHTML = `Please fill out the Home Price, Mortgage Period & Interest!`;
   }
   });
 
